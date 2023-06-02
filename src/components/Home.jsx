015f24a06801ea8d43e11f7db9f5f975a83d0d49
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-06-02 23:53:51 +0100

    Adding Home sction

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import MyImg from '../assets/my-pic-with-3D-icons.jpg'
+
 
 const Home = ()=>{
 
     return(
-        <div>Home section</div>
+        <div className="w-full flex flex-col max-w-[1240px] text-center items-center md:flex-row justify-between mx-auto text-left items-start">
+            <div>
+                <h4 className="text-white text-xl mb-6 md:text-3xl">Hey there! my name is</h4>
+                <h2 className="text-white text-4xl font-bold my-1 md:text-6xl">Nefis <span className="text-[#facd00]">Abdelkrim</span></h2>
+                <h2 className="text-[#facd00] text-3xl font-bold my-1 md:text-5xl">Front-End <span className="text-white">Developer</span></h2>
+                <div className="bg-white py-0.5 px-auto rounded-3xl border-[#fadc00] mt-10 outline-yellow-500">
+                    <input className="pl-8 py-2 focus:outline-none rounded-full" type="email" placeholder="Put your email here"/>
+                    <button className="bg-[#facd00] py-2 px-8 rounded-full drop-shadow-md">Send</button>
+                </div>
+            </div>
+            <div>
+                <img className="w-[450px] md:w-[600px]" src={MyImg} alt="" />
+            </div>
+        </div>
     )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
             <div onClick={handelClick} className="cursor-pointer block mr-6 md:hidden">
                 {!nav ? <FontAwesomeIcon icon={faBars} style={{color: "#facd00",}} size="2xl" /> : <FontAwesomeIcon icon={faX} style={{color: "#fadc00",}} size="2xl" />}
             </div>
-            <div className={!nav ? "fixed left-[-100%]" : "fixed left-0 top-0 w-[70%] border-r h-full border-r-gray-900 bg-[#000300] ease-in-out duration-500"}>
+            <div className={!nav ? "fixed left-[-100%]" : "fixed left-0 top-0 w-[75%] border-r h-full border-r-gray-900 bg-[#000300] ease-in-out duration-500"}>
             <h1 className="w-full text-3xl font-bold text-[#FACD00] mt-7 mb-4 mx-4">Nefis <span className="text-white">Abdelkrim</span></h1>
                 <ul className="">
                     <li className="text-center py-4 cursor-pointer hover:-translate-y-1 hover:scale-100 hover:bg-[#FADC00] hover:text-black hover:text-xl border-b border-gray-600 duration-300 text-center">Home</li>
