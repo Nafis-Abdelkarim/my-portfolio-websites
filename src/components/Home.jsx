7da53730d12e018543693d4806935f4ede145fe8
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-06-04 14:56:57 +0100

    Updating the hme section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,20 @@ import MyImg from '../assets/my-pic-with-3D-icons.jpg'
 
 const Home = ()=>{
 
+    const handleDownloadMyCv = () => {
+        const fileUrl = "my-protofolio-website/src/assets/Nefis Abdelkrim junior Front-End developer cv 2023.pdf";
+        window.location.href = fileUrl;
+    }
+
     return(
         <div className="w-full flex flex-col max-w-[1240px] text-center items-center md:flex-row justify-between mx-auto text-left items-start">
-            <div>
+            <div className="my-6">
                 <h4 className="text-white text-xl mb-6 md:text-3xl md:text-left">Hey there! my name is</h4>
                 <h2 className="text-white text-4xl font-bold my-1 md:text-6xl md:text-left">Nefis <span className="text-[#facd00]">Abdelkrim</span></h2>
                 <h2 className="text-[#facd00] text-3xl font-bold my-1 md:text-5xl md:text-left">Front-End <span className="text-white">Developer</span></h2>
-                <div className="bg-white border-[#facd00] py-0.5 rounded-3xl mt-10 md: mt-2">
-                    <input className="outline-none rounded-full text-lg md:mx-0" type="email" placeholder="Put your email here"/>
-                    <button className="bg-[#facd00] py-2 px-8 rounded-full drop-shadow-md md:ml-20">Send</button>
+                <div className="mx-2">
+                    <h4 className="text-white font-bold text-xl mb-6 md:text-3xl">Get my <span className="text-[#facd00]">CV !</span></h4>
+                    <a href="#" className="bg-[#facd00] px-8 py-3 rounded-full hover:bg-black hover:text-[#facd00] hover:border-2 border-yellow-500 hover:cursor-pointer mx-2" onClick={handleDownloadMyCv}>Download</a>
                 </div>
             </div>
             <div>
